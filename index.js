8d6d62ae8de03da9e473c77816b5bcc92fab10cc
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2022-05-28 22:36:50 +0600

    user order delete api

--- index.js
+++ index.js
@@ -59,6 +59,15 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products)
         })
+        app.get('/order', async (req, res) => {
+
+        })
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
 
