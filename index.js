816496738b855f85ea559167f7089f92da5b4a97
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2022-05-29 04:33:34 +0600

    final check

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ async function run() {
         });
 
 
-        app.get('/products/:id', async (req, res) => {
+        app.get('/products/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const product = await productsCollection.findOne(query);
